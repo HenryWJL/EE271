d8485b8374168b061bddfebc901e1d44bc5f78d6
JunlinWang <114377159+HenryWJL@users.noreply.github.com>
2022-12-04 12:33:05 +0800

    Update origin_KNN.py

--- project1/origin_KNN.py
+++ project1/origin_KNN.py
@@ -3,10 +3,10 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from sklearn.neighbors import KNeighborsClassifier
 
-# Please revise the absolute path below so that the program can run on your computer.
+# If you can't find the data files, please revise the file path below.
 
-train_data_path='/home/txke/Python_codes/project1/origin_breast_cancer_data.csv'
-test_data_path='/home/txke/Python_codes/project1/breast_cancer_data_357B_100M.csv'
+train_data_path='origin_breast_cancer_data.csv'
+test_data_path='breast_cancer_data_357B_100M.csv'
 
 class origin_breast_cancer_dataset(torch.utils.data.Dataset):
     def __init__(self,*args,**kwargs):
